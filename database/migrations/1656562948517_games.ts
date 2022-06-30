da0b8a77a4340247f7436fe177f6b50943bd89c2
Ives Moreira Gomes <ives.gomes@acensjr.com>
2022-06-30 01:38:42 -0300

    feat: create migrations

---
+++ database/migrations/1656562948517_games.ts
@@ -0,0 +1,25 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'games'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').unsigned()
+
+      table.string('name', 50).notNullable()
+      table.string('description', 250).notNullable()
+      table.integer('range').unsigned().notNullable()
+      table.decimal('price', 8, 2).defaultTo(0).unsigned().notNullable()
+      table.integer('min_and_max_value').unsigned().notNullable()
+      table.string('color', 50).notNullable()
+
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
